00360e36a32e17a59edd837dc95ba141549d99bc
nar2p1nk <farrisisqandar@gmail.com>
2022-03-29 22:18:37 +0800

    recreating backend to have jwt authentication and using vue as frontend

---
+++ frontend/src/components/todoList.vue
@@ -0,0 +1,41 @@
+
+<script>
+export default {
+    props:{todos:Array}
+}
+</script>
+
+<template>
+    <div class="list">
+        <h2></h2>
+        <form action="">
+            <ul>
+                <li v-for="item in todos" :key='item.id'>
+                    <h3>
+                        <label for="item.id">{{item.todo}}</label>
+                        <input type="checkbox" name='item.todo'
+                            value='item.id'>
+                    </h3>
+                </li>
+            </ul>
+            <button type='button' class='check'>check tasks</button>
+        </form>
+    </div>
+</template>
+
+<style>
+ul{
+    padding: 0;
+    list-style: none;
+}
+li{
+    width: 100%;
+}
+label{
+    display: inline-block;
+    width: 200px;
+}
+.check{
+    float: right;
+}
+</style>
---
+++ frontend/src/main.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue';
+import todoApp from './todoApp.vue'
+
+createApp(todoApp).mount('#app')
