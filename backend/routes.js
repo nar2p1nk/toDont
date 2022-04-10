6fad80ab4025e964d493055433ec031c27ae0a15
nar2p1nk <farrisisqandar@gmail.com>
2022-04-10 15:33:44 +0800

    trying to fix bug on laptop

--- backend/routes.js
+++ backend/routes.js
@@ -1,33 +1,13 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const express = require('express')
 const passport = require('passport');
-const jwt = require('jsonwebtoken');
-require('./model');
-require('./auth')
-const expressjwt = require('express-jwt');
-const sqlite = require('better-sqlite3');
-const routes = require('./routes');
-const app = express();
-
-//app.use(expressjwt({secret:'gila',algorithms:['HS256']}))
-
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json())
-
-app.use('/routes',routes)
+const router = express.Router()
 
 
-app.get('/',(req,res)=>{
-    const token = jwt.sign('egg', 'gila')
-    res.json({token})
-})
-
-
-app.post(
+router.post(
     '/login',
     (req,res,next)=>{
         passport.authenticate(
-            'logan',
+            'login',
             (err,user,info)=>{
                 try{
                     console.log('trying')
@@ -56,8 +36,4 @@ app.post(
     }
 )
 
-
-
-app.listen(8080,()=>{
-    console.log('listening at localhost:8080/')
-})
+module.exports = router
