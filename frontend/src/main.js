1176e4ddb0fb264e4af8bdfadbdd1c0540d2aa38
nar2p1nk <farrisisqandar@gmail.com>
2022-04-17 02:33:58 +0800

    created _helper

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue';
 import router from './router';
 
 import {createApp} from 'vue';
+import store from './store'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -22,12 +22,28 @@ const routes = [
     path: '/todos',
     name: 'todos',
     component:todoList
-  }
+  },
+  {path:'*',redirect:'/'}
 ]
 
 const router = createRouter({
+  mode:'history',
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to,from,next)=>{
+  // redirect to login page if not logged in and
+  // trying to access a restricted page
+  const publicPages = ['/login'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+
+  if(authRequired && !loggedIn){
+    return next('/login');
+  }
+
+  next();
 })
 
 export default router
