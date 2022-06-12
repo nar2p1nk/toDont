47cfa11f1818eac742dbc66f01a6900d7d4748e0
nar2p1nk <farrisisqandar@gmail.com>
2022-06-12 13:50:14 +0800

    creating a react version of this

--- frontend/src/main.js
+++
@@ -1,8 +0,0 @@
-//import todoApp from './todoApp.vue';
-import App from './App.vue';
-import router from './router';
-
-import {createApp} from 'vue';
-import store from './store'
-
-createApp(App).use(store).use(router).mount('#app')
