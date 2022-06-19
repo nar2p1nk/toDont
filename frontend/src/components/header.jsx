2cbe7838185b85af891535c4d2d7f1d04ebcb6c5
nar2p1nk <farrisisqandar@gmail.com>
2022-06-19 23:38:52 +0800

    styling 'header.jsx'

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,7 +1,27 @@
+import './style/header.css';
 
 const Header = () =>{
     return(
-        <h1>Header</h1>
+        <div className="nav">
+            <h2 className='title'>To-Don't</h2>
+            <ul className='nav-list'>
+                    <li className="nav-item Home">
+                        <a href='#Home' className='nav-link Home'>
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item Main">
+                        <a href='#Main' className='nav-link Main'>
+                            Main
+                        </a>
+                    </li>
+                <li className="nav-item About">
+                    <a href='#About' className='nav-link About'>
+                    About
+                    </a>
+                </li>
+            </ul>
+        </div>
     )
 }
 
