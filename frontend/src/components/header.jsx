97206449328c7e1f339905e9f56e027c47d6d71a
nar2p1nk <farrisisqandar@gmail.com>
2022-06-20 23:49:33 +0800

    trying to make header smaller

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -5,19 +5,28 @@ const Header = () =>{
         <div className="nav">
             <h2 className='title'>To-Don't</h2>
             <ul className='nav-list'>
+                        
                     <li className="nav-item Home">
+                        <p>
                         <a href='#Home' className='nav-link Home'>
                             Home
                         </a>
+                        </p>
                     </li>
+                        
                     <li className="nav-item Main">
+                        <p>
                         <a href='#Main' className='nav-link Main'>
                             Main
                         </a>
+                        </p>
                     </li>
                 <li className="nav-item About">
+                    
                     <a href='#About' className='nav-link About'>
+                    <p>
                     About
+                    </p>
                     </a>
                 </li>
             </ul>
