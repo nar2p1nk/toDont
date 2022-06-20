1378cab5f34dddc3deb4c204ff0a032960b272fa
nar2p1nk <farrisisqandar@gmail.com>
2022-06-21 00:55:42 +0800

    done styling navigation bar (mistakenly called 'header')

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -3,33 +3,12 @@ import './style/header.css';
 const Header = () =>{
     return(
         <div className="nav">
-            <h2 className='title'>To-Don't</h2>
-            <ul className='nav-list'>
-                        
-                    <li className="nav-item Home">
-                        <p>
-                        <a href='#Home' className='nav-link Home'>
-                            Home
-                        </a>
-                        </p>
-                    </li>
-                        
-                    <li className="nav-item Main">
-                        <p>
-                        <a href='#Main' className='nav-link Main'>
-                            Main
-                        </a>
-                        </p>
-                    </li>
-                <li className="nav-item About">
-                    
-                    <a href='#About' className='nav-link About'>
-                    <p>
-                    About
-                    </p>
-                    </a>
-                </li>
-            </ul>
+            <a className='title' href='#Landing'>To-Don't</a>
+            <div className="navbar">
+                <a className="nav-item Home" href="#Home">Home</a>
+                <a className="nav-item Contact" href="#Contact">Contact</a>
+                <a className="nav-item About" href="#About">About</a>
+            </div>
         </div>
     )
 }
