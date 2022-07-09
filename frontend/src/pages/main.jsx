038c7379d992e49e99356a2546e3cd16504dc69f
nar2p1nk <farrisisqandar@gmail.com>
2022-07-09 17:06:56 +0800

    added input div for completed

--- frontend/src/pages/main.jsx
+++ frontend/src/pages/main.jsx
@@ -38,23 +38,25 @@ useEffect(()=>{
                 </div>
                 <div className='todos'>
                     <div className="uncompleted-div">
-                        <div className="inputDiv">
+                        <div className="inputDiv uncompleted">
                             <input className="input todo"
                                 placeholder='enter todo'
                                 type="text" />
                             <br/>
                             <button className="button todo">Post</button>
                             <button className="button todo">
-                            Complete todo
+                            Complete selected todos
                             </button>
                             <button className="button todo">Check all</button>
                         </div>
+                        <div className='list-todosUncompleted'>
                         {todos.map(todo =>{
                             if(todo.completed === 1){return null;}
                             return(
                                 <div className='todo completed' key={todo.todoId}><p>{todo.todo}</p></div>
                             )
                         })}
+                        </div>
                     </div>
                 <div className='completed-div'>
                 {todos.map(todo =>{
@@ -63,6 +65,10 @@ useEffect(()=>{
                         <div className='todo uncompleted' key={todo.todoId}><p>{todo.todo}</p></div>
                     )
                 })}
+                    <div className="inputDiv completed">
+                        <button className="button todo">Delete selected todos</button>
+                        <button className="button todo">Check all</button>
+                    </div>
                 </div>
                 </div>
         </div>
