63dc54a0a762d9f815affe0725e7879bafdb73d7
nar2p1nk <farrisisqandar@gmail.com>
2022-07-16 21:38:58 +0800

    created a jsx file for a home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header';
 import Main from './pages/main';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import Home from './pages/home.jsx';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <BrowserRouter>
       <Header/>
       <Routes>
+        <Route path='/home' element={<Home/>} />
         <Route path='/' element={<Main/>} />
         <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />}/>
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,10 @@
+const home = () => {
+    return (
+        <div class="homepage">
+            <h1>welcome to toDon't</h1>
+        </div>
+    )
+}
+
+export default home
+
