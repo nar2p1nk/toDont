041227d5f77e5682b5f481aea3a2f22fac83c342
nar2p1nk <farrisisqandar@gmail.com>
2022-07-18 21:25:06 +0800

    added a button for the landing section of homepage

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,29 +1,21 @@
-import {useEffect} from "react";
 import './style/home.css';
-import {useNavigate} from "react-router-dom";
 
 
 const Home = () => {
-    const navigate = useNavigate();
-
-    useEffect(()=>{
-        const token = sessionStorage.getItem('jwtToken');
-        if(token === null){
-            navigate('/login')
-            return;
-        }
-    })
     return (
         <div class="homepage">
-            <div className='landing div'>
+            <div className='section landing div'>
             <div className='landing content'>
             <p className='landing text'>welcome to to-Don't!</p>
                 <p className="landing description">
                     Where instead of making a simple todo app for your first project,
-                    you make a highly complex glorified "to-Don't" web app with beautiful design.
+                    you made a highly complex glorified "to-Don't" web app with beautiful
+                    design(i think) as a second big project.
                 </p>
+                <button className="landing button">Check it out now!</button>
             </div>
             </div>
+            <div className="section features div"></div>
         </div>
     )
 }
